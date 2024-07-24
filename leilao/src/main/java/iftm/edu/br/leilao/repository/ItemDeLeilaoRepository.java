0de7847a41e7694edb2b52a39359017e834212eb
Lucas <108193174+lucas-azv@users.noreply.github.com>
2024-07-23 21:52:26 -0300

    Add files via upload

---
+++ leilao/src/main/java/iftm/edu/br/leilao/repository/ItemDeLeilaoRepository.java
@@ -0,0 +1,8 @@
+package iftm.edu.br.leilao.repository;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class ItemDeLeilaoRepository {
+    
+}
---
+++ leilao/src/main/java/iftm/edu/br/leilao/repository/ParticipanteRepository.java
@@ -0,0 +1,8 @@
+package iftm.edu.br.leilao.repository;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class ParticipanteRepository extends CrudRepository<Lance,Long>{
+    
+}
